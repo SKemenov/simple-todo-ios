269760e5dff69c10ede48fccfe291c39ad13b7c7
Sergey Kemenov <skemenov@icloud.com>
2026-02-05 16:07:56 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Sergey Kemenov on 05.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
