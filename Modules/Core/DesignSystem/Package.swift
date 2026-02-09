4fd09ea1b9da3170aade99b14310ed8a4b002411
Sergey Kemenov <skemenov@icloud.com>
2026-02-09 11:14:33 +0300

    create `Core` SPM packages

---
+++ Modules/Core/DesignSystem/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DesignSystem",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(
+            name: "DesignSystem",
+            targets: ["DesignSystem"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "DesignSystem",
+            path: "Sources/DesignSystem",
+            resources: [
+            ],
+            swiftSettings: [.swiftLanguageMode(.v5)],
+        ),
+        .testTarget(
+            name: "DesignSystemTests",
+            dependencies: ["DesignSystem"]
+        ),
+    ]
+)
--- ToDoList/App.swift
+++ ToDoList/App.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ToDoFeature
 
 @main
 struct ToDoListApp: App {
