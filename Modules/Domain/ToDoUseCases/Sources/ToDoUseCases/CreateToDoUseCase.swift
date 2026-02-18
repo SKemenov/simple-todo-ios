d6cd7709c217e4df0931bfe3e3b4429c07804856
Sergey Kemenov <skemenov@icloud.com>
2026-02-19 00:16:01 +0300

    Create `Domain` use cases

---
+++ Modules/Domain/ToDoUseCases/Sources/ToDoUseCases/CreateToDoUseCase.swift
@@ -0,0 +1,35 @@
+//
+//  CreateToDoUseCase.swift
+//  ToDoUseCases
+//
+//  Created by Sergey Kemenov on 10.02.2026.
+//
+
+import Foundation
+import DomainInterface
+
+public final class CreateToDoUseCase: CreateToDoUseCaseProtocol {
+    private let repository: ToDoRepositoryProtocol
+    private let validator = ToDoDomainValidator()
+
+    public init(repository: ToDoRepositoryProtocol) {
+        self.repository = repository
+    }
+
+    public func execute(title: String, description: String) async throws {
+        var checkedTitle = validator.validateTitle(title)
+        var checkedDescription = validator.validateDescription(description)
+
+        if checkedTitle.isEmpty {
+            checkedTitle = checkedDescription
+            checkedDescription = ""
+        }
+
+        let toDo = DomainModel.ToDo(
+            id: repository.nextToDoId,
+            todoTitle: checkedTitle,
+            todoDescription: checkedDescription
+        )
+        try await repository.createToDo(toDo)
+    }
+}
