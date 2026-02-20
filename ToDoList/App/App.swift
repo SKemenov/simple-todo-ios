a874050eabf3f8c04126e907c1ab35df2a07ce25
Sergey Kemenov <skemenov@icloud.com>
2026-02-20 16:55:16 +0300

    Create `ToDoList` test plan

--- ToDoList/App/App.swift
+++ ToDoList/App/App.swift
@@ -15,26 +15,27 @@ import LocalStores
 
 @main
 struct ToDoListApp: App {
-    @StateObject private var dependencyContainer: DependencyContainer
+    private(set) var dependencyContainer: DependencyContainer
     @StateObject private var coordinator: AppCoordinator
     private let persistence: PersistenceController
 
     init () {
         let container = DependencyContainer()
-        _dependencyContainer = StateObject(wrappedValue: container)
-        _coordinator = StateObject(wrappedValue: AppCoordinator(dependencyContainer: container))
+        dependencyContainer = container
+        _coordinator = StateObject(wrappedValue: AppCoordinator(container: container))
         persistence = container.persistenceController
-        UIView.appearance().tintColor = UIColor(Color.designSystem(.text(.accent)))
-        Logger.core.info("\(Current.logHeader()) App started")
+        Logger.core.info("\(String.logHeader()) App started")
     }
 
     var body: some Scene {
         WindowGroup {
             AppCoordinatorView()
                 .environmentObject(coordinator)
-                .environmentObject(dependencyContainer)
-                .environment(\.managedObjectContext, persistence.container.viewContext)
+                .environment(\.managedObjectContext, persistence.mainViewContext)
                 .preferredColorScheme(.dark)
+                .onAppear {
+                    UIView.appearance().tintColor = UIColor(Color.designSystem(.text(.accent)))
+                }
         }
     }
 }
