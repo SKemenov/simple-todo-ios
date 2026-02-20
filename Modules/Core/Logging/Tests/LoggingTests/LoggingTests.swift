2af0a3b34062bea986b294ee8a737098762002d5
Sergey Kemenov <skemenov@icloud.com>
2026-02-20 16:44:24 +0300

    Create `Logging` tests

--- Modules/Core/Logging/Tests/LoggingTests/LoggingTests.swift
+++ Modules/Core/Logging/Tests/LoggingTests/LoggingTests.swift
@@ -1,6 +1,37 @@
 import Testing
 @testable import Logging
+import Foundation
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Suite("Logging tests")
+struct LoggingTests {
+
+    @Test("Can log [CORE] submodule messages")
+    func logCoreMessage() {
+        Logger.core.log("test")
+        #expect(true)
+    }
+
+    @Test("Can log [NETWORK] submodule messages")
+    func logNetworkMessage() {
+        Logger.network.log("test")
+        #expect(true)
+    }
+
+    @Test("Can log [STORAGE] submodule messages")
+    func logStorageMessage() {
+        Logger.storage.log("test")
+        #expect(true)
+    }
+
+    @Test("Can log [REPOS] submodule messages")
+    func logRepoMessage() {
+        Logger.repos.log("test")
+        #expect(true)
+    }
+
+    @Test("Can log [USER_FLOW] submodule messages")
+    func logUserFlowMessage() {
+        Logger.userFlow.log("test")
+        #expect(true)
+    }
 }
