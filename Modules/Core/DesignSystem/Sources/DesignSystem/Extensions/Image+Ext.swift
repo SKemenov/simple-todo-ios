a0afbe41d4d3e12ea55361955f05a7461f13f890
Sergey Kemenov <skemenov@icloud.com>
2026-02-28 10:51:41 +0300

    update DesignSystem Image Extensions

--- Modules/Core/DesignSystem/Sources/DesignSystem/Extensions/Image+Ext.swift
+++ Modules/Core/DesignSystem/Sources/DesignSystem/Extensions/Image+Ext.swift
@@ -19,35 +19,35 @@ extension Image {
 
 extension Image.DS.Icons: CaseIterable {
     /// SF symbol for "checkmark.circle"
-    public static let complete: Image = Image(systemName: .DS.Icons.complete)
+    public static let complete = Image(systemName: .DS.Icons.complete)
 
     /// SF symbol for "circle"
-    public static let incomplete: Image = Image(systemName: .DS.Icons.incomplete)
+    public static let incomplete = Image(systemName: .DS.Icons.incomplete)
 
     /// SF symbol for "square.and.pencil"
-    public static let create: Image = Image(systemName: .DS.Icons.create)
+    public static let create = Image(systemName: .DS.Icons.create)
 
     /// SF symbol for "square.and.arrow.up"
-    public static let share: Image = Image(systemName: .DS.Icons.share)
+    public static let share = Image(systemName: .DS.Icons.share)
 
     /// SF symbol for "long.text.page.and.pencil"
-    public static let edit: Image = Image(systemName: .DS.Icons.edit)
+    public static let edit = Image(systemName: .DS.Icons.edit)
 
     /// SF symbol for "trash"
-    public static let delete: Image = Image(systemName: .DS.Icons.delete)
+    public static let delete = Image(systemName: .DS.Icons.delete)
 
     /// SF symbol for "microphone.fill"
-    public static let dictate: Image = Image(systemName: .DS.Icons.dictate)
+    public static let dictate = Image(systemName: .DS.Icons.dictate)
 
     /// SF symbol for "magnifyingglass"
-    public static let search: Image = Image(systemName: .DS.Icons.search)
+    public static let search = Image(systemName: .DS.Icons.search)
 
     /// SF symbol for "exclamationmark.triangle"
-    public static let warning: Image = Image(systemName: .DS.Icons.warning)
+    public static let warning = Image(systemName: .DS.Icons.warning)
 
     /// SF symbol for "doc.text"
-    public static let empty: Image = Image(systemName: .DS.Icons.empty)
+    public static let empty = Image(systemName: .DS.Icons.empty)
 
     /// SF symbol for "xmark.circle.fill"
-    public static let close: Image = Image(systemName: .DS.Icons.close)
+    public static let close = Image(systemName: .DS.Icons.close)
 }
