df5dd9eb60447e056e9caec4bc1409b1b540ccf1
Sergey Kemenov <skemenov@icloud.com>
2026-03-14 13:14:00 +0300

    update `root` screen VM

--- Modules/Presentation/ToDoFeature/Sources/ToDoFeature/Screens/RootScreen/RootViewModel.swift
+++ Modules/Presentation/ToDoFeature/Sources/ToDoFeature/Screens/RootScreen/RootViewModel.swift
@@ -15,26 +15,49 @@ public final class RootViewModel: ObservableObject {
 
     private let getAllToDosUseCase: GetAllToDosUseCaseProtocol
     private let clearCacheToDoUseCase: ClearCacheToDoUseCaseProtocol
+    private let errorManager: ErrorManager
+    private let networkState: NetworkState
 
     public init(
         getAllToDosUseCase: GetAllToDosUseCaseProtocol,
-        clearCacheToDoUseCase: ClearCacheToDoUseCaseProtocol
+        clearCacheToDoUseCase: ClearCacheToDoUseCaseProtocol,
+        errorManager: ErrorManager,
+        networkState: NetworkState
     ) {
         self.getAllToDosUseCase = getAllToDosUseCase
         self.clearCacheToDoUseCase = clearCacheToDoUseCase
+        self.errorManager = errorManager
+        self.networkState = networkState
     }
 
     @MainActor
     func loadData() async {
-        guard let todos = try? await getAllToDosUseCase.execute() else { return }
-        Logger.userFlow.info("\(String.logHeader()) Loaded. Local DataStore has [\(todos.count)] records")
-        totalCount = todos.count
+        guard !networkState.isLoading else { return }
+        networkState.set(true, for: String.logHeader())
+        defer { networkState.set(false, for: String.logHeader()) }
+
+        do {
+            let todos = try await getAllToDosUseCase.execute()
+            Logger.userFlow.info("\(String.logHeader()) Loaded. Local DataStore has [\(todos.count)] records")
+            totalCount = todos.count
+        } catch {
+            errorManager.show("Failed to load data.", kind: .init(from: error))
+            Logger.userFlow.error("\(String.logHeader()) Loading error: \(error)")
+        }
     }
 
     @MainActor
     func clearCache() async {
-        try? await clearCacheToDoUseCase.execute()
-        Logger.userFlow.info("\(String.logHeader()) Deleted all records")
-        totalCount = 0
+        guard !networkState.isLoading else { return }
+        networkState.set(true, for: String.logHeader())
+        defer { networkState.set(false, for: String.logHeader()) }
+        do {
+            try await clearCacheToDoUseCase.execute()
+            Logger.userFlow.info("\(String.logHeader()) Deleted all records")
+            totalCount = 0
+        } catch {
+            errorManager.show("Failed to clear cache.", kind: .init(from: error))
+            Logger.userFlow.error("\(String.logHeader()) Clear cache error: \(error)")
+        }
     }
 }
